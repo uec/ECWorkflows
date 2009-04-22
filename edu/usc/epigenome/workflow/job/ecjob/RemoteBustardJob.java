e1b37edf411dc8e61b69fbcfc10067f5ec0a5cd2
zack <zack@fbcdd564-4565-0410-a881-3544ea04ba27>
2009-04-22 18:52:10 +0000

    restructuring, no more staging, use FILE to setup symlinks. two main entry points
    git-svn-id: https://svn.code.sf.net/p/uscecr/code/WorkflowDAX/src@20 fbcdd564-4565-0410-a881-3544ea04ba27

---
+++ edu/usc/epigenome/workflow/job/ecjob/RemoteBustardJob.java
@@ -0,0 +1,25 @@
+package edu.usc.epigenome.workflow.job.ecjob;
+
+import org.griphyn.vdl.dax.PseudoText;
+
+import edu.usc.epigenome.workflow.DAX.WorkflowConstants;
+import edu.usc.epigenome.workflow.job.ECJob;
+
+public class RemoteBustardJob extends ECJob
+{
+	public RemoteBustardJob(String flowcell, String[] genomes, String referenceLane, String copyBackTmpDir, String copyBackUsername, String copyToUsername, String webdir)
+	{
+		super(WorkflowConstants.NAMESPACE, "remotebustard", WorkflowConstants.VERSION, "remotebustard_" + flowcell);
+		for(String s : genomes)
+		{
+			if(s != null)
+				this.addArgument(new PseudoText(s + " "));
+		}
+		this.addArgument(new PseudoText("flowcell=" + flowcell + " "));
+		this.addArgument(new PseudoText("remotetmpdir=" + copyBackTmpDir + " "));
+		this.addArgument(new PseudoText("username=" + copyBackUsername + " "));
+		this.addArgument(new PseudoText("remoteuser=" + copyToUsername + " "));
+		this.addArgument(new PseudoText("Eland.webdir=" + webdir + " "));
+		this.addArgument(new PseudoText("--GERALD=config.txt --with-prb --with-seq --control-lane="+ referenceLane + " --make ./Data/IPAR_1.3"));
+	}
+}
