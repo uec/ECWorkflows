7c95eaa8e769074ee8fba95ba5e4ad13302e51e1
zack <zack@fbcdd564-4565-0410-a881-3544ea04ba27>
2009-07-08 00:15:21 +0000

    added support for pulling xml from geneus
    use constant splitting factor
    
    git-svn-id: https://svn.code.sf.net/p/uscecr/code/trunk/WorkflowDAX@50 fbcdd564-4565-0410-a881-3544ea04ba27

--- src/edu/usc/epigenome/workflow/ReportFromPileup.java
+++ src/edu/usc/epigenome/workflow/ReportFromPileup.java
@@ -125,16 +125,22 @@ public class ReportFromPileup
 	
 	public static void usage()
 	{
-		System.out.println("Error: parameter file does not exist");
-		System.out.println("Usage: program [-dryrun] [-pbs] workflowParameterFile.txt");
+		System.out.println("Error: parameter file does not exist and no valid URL given");
+		System.out.println("Usage: program [-dryrun] [-pbs] [workflowParameterFile.txt] [http://processURL]");
 		System.out.println("workflowParameterFile.txt: contains all parameters");
+		System.out.println("workflowURL.txt: contains all parameters");
 		System.out.println("-pbs: operate in pbs mode");
 		System.out.println("-dryrun: display pbs output, do not run");
+		System.err.println("Either one of the arguements or both must be specified");
 		System.exit(0);
 	}
+	/**
+	 * @param args input parameter filename to use
+	 */
 	public static void main(String[] args)
 	{
 		String paramFile = "";
+		String processURL = "";
 		Boolean dryrun = false;
 		Boolean pbsMode = false;
 		
@@ -146,16 +152,29 @@ public class ReportFromPileup
 				pbsMode = true;
 			else if(new File(s).exists())
 				paramFile = s;
+			else if(s.contains("http://"))
+				processURL = s;
 			else
 				usage();
-		}		
-				
-		ECDax dax = new ECDax(new ECWorkflowParams(paramFile));
+		}
+		
+		ECWorkflowParams par = null;
+		if(paramFile.length() > 0 && processURL.length() > 7)
+			par = new ECWorkflowParams(new File(paramFile), processURL);
+		else if(paramFile.length() > 0 && processURL.length() == 0)
+			par = new ECWorkflowParams(new File(paramFile));
+		else if(paramFile.length() == 0 && processURL.length() > 7)
+			par = new ECWorkflowParams(processURL);
+		else
+			usage();
+		
+		ECDax dax = new ECDax(par);
 		createWorkFlow(dax, pbsMode);
-		dax.saveAsDot("reportonly_dax.dot");
-		dax.saveAsSimpleDot("reportonly_dax_simple.dot");
+		dax.saveAsDot("alignpileup_dax.dot");
+		dax.saveAsSimpleDot("alignpileup_dax_simple.dot");
 		if(pbsMode)
 			dax.runWorkflow(dryrun);
-		dax.saveAsXML("reportonly_dax.xml");
+		dax.saveAsXML("alignpileup_dax.xml");		
 	}
+
 }
