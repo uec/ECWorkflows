ca0a63bddafd0014da25af7943fc922f44c1a83b
zack <zack@fbcdd564-4565-0410-a881-3544ea04ba27>
2009-07-27 23:53:28 +0000

    added new workflow: ReportFromMaps. generates reports from a set of maps, by merging, pileup, then java report
    Updated README
    
    git-svn-id: https://svn.code.sf.net/p/uscecr/code/trunk@55 fbcdd564-4565-0410-a881-3544ea04ba27

--- WorkflowDAX/src/edu/usc/epigenome/workflow/job/ecjob/MapMergeJob.java
+++ WorkflowDAX/src/edu/usc/epigenome/workflow/job/ecjob/MapMergeJob.java
@@ -1,6 +1,8 @@
 package edu.usc.epigenome.workflow.job.ecjob;
 
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import org.griphyn.vdl.classes.LFN;
 import org.griphyn.vdl.dax.Filename;
@@ -37,5 +39,28 @@ public class MapMergeJob extends ECJob
 					this.addArgument(new PseudoText(" "));				
 			}
 		}
-	}	
+	}
+	public MapMergeJob(ArrayList<String> mapJobs, String mergedName)
+	{
+		super(WorkflowConstants.NAMESPACE, "mapMerge", WorkflowConstants.VERSION, "mapMerge_" + mergedName);
+		// only one output file
+		String outputFileNameMap = mergedName + "_merged.map";
+		Filename outputFileMap = new Filename(outputFileNameMap, LFN.OUTPUT);
+		outputFileMap.setRegister(true);
+		this.addUses(outputFileMap);
+
+		// add the arguments to the job
+		this.addArgument(outputFileMap);
+		this.addArgument(new PseudoText(" "));
+		// iterate through all the map jobs
+
+		for (String f : mapJobs)
+		{
+				Filename input = new Filename(f, LFN.INPUT);
+				input.setRegister(false);
+				this.addUses(input);
+				this.addArgument(input);
+				this.addArgument(new PseudoText(" "));				
+		}		
+	}
 }
