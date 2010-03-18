7f3b0c13b73c12fb167de9c7d69b0a971d335d7d
zack <zack@fbcdd564-4565-0410-a881-3544ea04ba27>
2010-03-18 18:35:22 +0000

    major changes to all workflows. full native support for PE new workflow for tophat-bowtie lots of refactoring
    git-svn-id: https://svn.code.sf.net/p/uscecr/code/trunk/WorkflowDAX@67 fbcdd564-4565-0410-a881-3544ea04ba27

--- src/edu/usc/epigenome/workflow/job/ecjob/FastQConstantSplitJob.java
+++ src/edu/usc/epigenome/workflow/job/ecjob/FastQConstantSplitJob.java
@@ -32,4 +32,39 @@ public class FastQConstantSplitJob extends ECJob
 		this.addArgument(new PseudoText(numberOfPieces + " "));
 		this.addArgument(input);
 	}
+	public FastQConstantSplitJob(String inputFileR1, String inputFileR2, int numberOfPieces) throws Exception
+	{
+		super(WorkflowConstants.NAMESPACE, "fastqConstantSplit", WorkflowConstants.VERSION, "fastqConstantSpit_" + new File(inputFileR1).getName());
+		Filename inputR1 = new Filename(inputFileR1, LFN.INPUT);
+		inputR1.setRegister(false);
+		this.addUses(inputR1);
+		
+		Filename inputR2 = new Filename(inputFileR2, LFN.INPUT);
+		inputR2.setRegister(false);
+		this.addUses(inputR2);
+
+		// construct the output filenames for job
+		for (int i = 1; i <= numberOfPieces; i++)
+		{
+			File inFileR1 = new File(inputFileR1);
+			File inFileR2 = new File(inputFileR2);
+			
+			String outFileR1 = new String(inFileR1.getName());
+			outFileR1 = outFileR1.replaceAll("^(.+?)(\\.\\w+)$", "$1\\." + i + "$2");
+			Filename outputR1 = new Filename(outFileR1, LFN.OUTPUT);
+			outputR1.setRegister(false);
+			this.addUses(outputR1);
+			
+			String outFileR2 = new String(inFileR2.getName());
+			outFileR2 = outFileR2.replaceAll("^(.+?)(\\.\\w+)$", "$1\\." + i + "$2");
+			Filename outputR2 = new Filename(outFileR2, LFN.OUTPUT);
+			outputR2.setRegister(false);
+			this.addUses(outputR2);
+		}
+		// add the arguments to the job
+		this.addArgument(new PseudoText(numberOfPieces + " "));
+		this.addArgument(inputR1);
+		this.addArgument(new PseudoText(" "));
+		this.addArgument(inputR2);
+	}
 }
