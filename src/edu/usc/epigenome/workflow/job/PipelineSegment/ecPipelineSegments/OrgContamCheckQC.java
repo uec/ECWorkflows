5cae4ebae0bc169c9209e4ca2ac67ca51e12a235
zack_hr <zack_hr@fbcdd564-4565-0410-a881-3544ea04ba27>
2013-01-25 20:37:38 +0000

    added contam check for salmon
    git-svn-id: https://svn.code.sf.net/p/uscecr/code/branches/sampleCentric@160 fbcdd564-4565-0410-a881-3544ea04ba27

--- src/edu/usc/epigenome/workflow/job/PipelineSegment/ecPipelineSegments/OrgContamCheckQC.java
+++ src/edu/usc/epigenome/workflow/job/PipelineSegment/ecPipelineSegments/OrgContamCheckQC.java
@@ -34,6 +34,7 @@ public class OrgContamCheckQC extends PipelineSegment
 				  "/home/uec-00/shared/production/genomes/mm9_unmasked/mm9_unmasked.fa",
 				  "/home/uec-00/shared/production/genomes/Ecoli/EcoliIHE3034.fa",
 				  "/home/uec-00/shared/production/genomes/rn4_unmasked/rn4.fa",
+				  "/home/uec-00/shared/production/genomes/salmon/salmosalar.fa",
 				  "/home/uec-00/shared/production/genomes/lambdaphage/NC_001416.fa"};
 		
 		OrgContamCheckJob bwaTestContam = new OrgContamCheckJob(laneInputFileNameR1,5000000,organisms);
