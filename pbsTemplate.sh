1c8daef26ba873c09ae1433bda917cb77960374e
zackramjan <zack.hr@gmail.com>
2015-05-11 13:57:10 -0400

    updated path in pbs template.

--- pbsTemplate.sh
+++ pbsTemplate.sh
@@ -1,12 +1,12 @@
 #PBS -q DAXPBS_QUEUE
 #PBS -S /bin/bash
-#PBS -l walltime=96:00:00
+#PBS -l walltime=900:00:00
 #DAXPBS_MEM
 #DAXPBS_CPU
 #Note: DAXPBS comments will be replaced with job specific data
 #DAXPBS_DEPS
-umask 022
-export PATH=$PATH:/home/uec-00/shared/production/software/pegasus/2.2.0/bin:/home/uec-00/shared/production/software/perl_utils_usc:/home/uec-00/shared/production/software/maq-0.7.1:/home/uec-00/shared/production/software/bin:/usr/bin:/bin:/usr/sbin:/sbin:/home/rcf-40/bberman/storage/bin:/home/uec-00/shared/production/software/tophat/default:/home/uec-00/shared/production/software/bowtie/default:/home/uec-00/shared/production/software/cufflinks/default:/home/uec-00/shared/production/software/samtools
+umask 002
+export PATH=/home//home/uec-00/shared/production/software/perl_utils_usc:/home/uec-00/shared/production/software/samtools:$PATH
 export CLASSPATH=/home/uec-00/shared/production/software/genomeLibs/apps-live.jar:/home/uec-00/shared/production/software/genomeLibs/biojava-live.jar:/home/uec-00/shared/production/software/genomeLibs/bytecode.jar:/home/uec-00/shared/production/software/genomeLibs/commons-cli.jar:/home/uec-00/shared/production/software/genomeLibs/commons-collections-2.1.jar:/home/uec-00/shared/production/software/genomeLibs/commons-dbcp-1.1.jar:/home/uec-00/shared/production/software/genomeLibs/commons-math-1.1.jar:/home/uec-00/shared/production/software/genomeLibs/commons-pool-1.1.jar:/home/uec-00/shared/production/software/genomeLibs/demos-live.jar:/home/uec-00/shared/production/software/genomeLibs/genomeLibs.jar:/home/uec-00/shared/production/software/genomeLibs/jgrapht-jdk1.5.jar:/home/uec-00/shared/production/software/genomeLibs/junit-4.4.jar:/home/uec-00/shared/production/software/genomeLibs/charts4j-1.2.jar:/home/uec-00/shared/production/software/genomeLibs/heatMap.jar:/home/uec-00/shared/production/software/genomeLibs/UscKeck.jar
 export RESULTS_DIR=DAXPBS_RESULTSDIR
 export TMP=DAXPBS_TMPDIR
