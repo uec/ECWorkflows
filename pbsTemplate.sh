5d5568cc4ab17757bf512d31ddd15e9a1df14228
Ramjan <zack.ramjan@login01.cm.cluster>
2015-10-15 11:59:46 -0400

    modified pbsTemplate and tc.data for better fit of resources

--- pbsTemplate.sh
+++ pbsTemplate.sh
@@ -18,3 +18,14 @@ cd $TMP/$PBS_JOBID
 #DAXPBS_COPYOUT
 cd $TMP
 rm -rf $PBS_JOBID
+#!/bin/sh
+echo "Job ID: $1"
+echo "User ID: $2"
+echo "Group ID: $3"
+echo "Job Name: $4"
+echo "Session ID: $5"
+echo "Resource List: $6"
+echo "Resources Used: $7"
+echo "Queue Name: $8"
+echo "Account String: $9"
+echo ""
